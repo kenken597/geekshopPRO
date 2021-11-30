798deda2112ca0f87bba005fecc6ddc8889f62ae
Nesterchuk Ivan <ivan597@rambler.ru>
2021-11-30 16:29:22 +0300

    add two controller and urls

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    re render(request, 'index.html')
+
+def products(request):
+    re render(request, 'products.html')
